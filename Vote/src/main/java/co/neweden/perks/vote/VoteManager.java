e7e9d97f34aee3369c921682ecd3f953164abf06
aaron <aaron@neweden.co>
2016-06-28 00:02:05 +0100

    Regular vote broadcasts

--- Vote/src/main/java/co/neweden/perks/vote/VoteManager.java
+++ Vote/src/main/java/co/neweden/perks/vote/VoteManager.java
@@ -15,16 +15,21 @@ import net.md_5.bungee.event.EventHandler;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 public class VoteManager implements Listener {
 
     private static Collection<VoteService> voteServices = new ArrayList<>();
-    private static Collection<String> recentVotes = new ArrayList<>();
+    private static Set<String> recentVotes = new HashSet<>();
     private static int recentTotalVotes = 0;
+
+    public VoteManager() {
+        Perks.getPlugion().getProxy().getScheduler().schedule(Perks.getPlugion(), () -> {
+            broadcastRecentVotes();
+        }, 0L, 1L, TimeUnit.MINUTES);
+    }
 
     public static Collection<VoteService> getVoteServices() { return new ArrayList<>(voteServices); }
 
@@ -80,8 +85,7 @@ public class VoteManager implements Listener {
         vs.incrementTotalVotes();
 
         if (event.getVote().getUsername() != null) {
-            if (!recentVotes.contains(event.getVote().getUsername()))
-                recentVotes.add(event.getVote().getUsername());
+            recentVotes.add(event.getVote().getUsername());
         }
         recentTotalVotes++;
 
@@ -92,17 +96,39 @@ public class VoteManager implements Listener {
         else
             uuid = Util.getUUID(event.getVote().getUsername());
 
-        if (uuid == null)
-            return;
+        if (uuid != null) {
 
-        Transaction t = Transactions.newTransaction(Transactions.Type.VOTE, uuid);
-        t.setVoteService(vs);
-        Perks.setBalance(uuid, Perks.getBalance(uuid) + vs.getCurrencyPerVote());
-        t.setStatus(Transactions.Status.COMPLETE);
-        if (player != null)
-            player.sendMessage(new TextComponent(Util.formatString("&aThanks for voting on '" + vs.getDisplayName() + "' you have earned " + Util.formatCurrency(vs.getCurrencyPerVote()) + ", type /vote to see where else you may be able to vote.")));
+            Transaction t = Transactions.newTransaction(Transactions.Type.VOTE, uuid);
+            t.setVoteService(vs);
+            Perks.setBalance(uuid, Perks.getBalance(uuid) + vs.getCurrencyPerVote());
+            t.setStatus(Transactions.Status.COMPLETE);
+
+            if (player != null)
+                player.sendMessage(new TextComponent(Util.formatString("&aThanks for voting on '" + vs.getDisplayName() + "' you have earned " + Util.formatCurrency(vs.getCurrencyPerVote()) + ", type /vote to see where else you may be able to vote.")));
+
+        }
+
+        if (recentTotalVotes >= 10)
+            broadcastRecentVotes();
     }
 
-    public static void clearLocalCache() { voteServices.clear(); }
+    private static void broadcastRecentVotes() {
+        if (recentTotalVotes < 1) return;
+        String message = "&a" + recentTotalVotes + " votes recently from ";
+        for (String name : recentVotes) {
+            message = message + name + ", ";
+        }
+        message = message.substring(0, message.length() - 2);
+        Perks.getPlugion().getProxy().broadcast(new TextComponent(Util.formatString(message)));
+
+        recentVotes.clear();
+        recentTotalVotes = 0;
+    }
+
+    public static void clearLocalCache() {
+        voteServices.clear();
+        recentVotes.clear();
+        recentTotalVotes = 0;
+    }
 
 }
