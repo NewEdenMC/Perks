7c90d5b2ca26d12649262a65c9d35fce66453838
aaron <aaron@neweden.co>
2016-06-29 19:57:19 +0100

    Forgot to add pweather in last commit

---
+++ Extras/src/main/java/co/neweden/perks/extras/cmd/PWeather.java
@@ -0,0 +1,12 @@
+package co.neweden.perks.extras.cmd;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class PWeather {
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        return true;
+    }
+
+}
