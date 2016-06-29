8f1004f79151d3dba7fa6cdbdb90afec58e29f7f
aaron <aaron@neweden.co>
2016-06-29 21:01:59 +0100

    Implemented Bro Fist and HIgh Five

--- Extras/src/main/java/co/neweden/perks/extras/Main.java
+++ Extras/src/main/java/co/neweden/perks/extras/Main.java
@@ -1,6 +1,8 @@
 package co.neweden.perks.extras;
 
 import co.neweden.perks.extras.cmd.BitchSlap;
+import co.neweden.perks.extras.cmd.BroFist;
+import co.neweden.perks.extras.cmd.HIghFive;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -11,6 +13,8 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         plugin = this;
         new BitchSlap();
+        new BroFist();
+        new HIghFive();
     }
 
     public static Main getPlugin() { return plugin; }
--- Extras/src/main/java/co/neweden/perks/extras/cmd/BroFist.java
+++ Extras/src/main/java/co/neweden/perks/extras/cmd/BroFist.java
@@ -1,11 +1,38 @@
 package co.neweden.perks.extras.cmd;
 
+import co.neweden.perks.extras.Main;
+import co.neweden.perks.extras.Util;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
-public class BroFist {
+public class BroFist implements CommandExecutor {
+
+    public BroFist() {
+        Main.getPlugin().getCommand("brofist").setExecutor(this);
+    }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage(Util.formatString("&cYou must specify an online player")); return true;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+        if (target == null) {
+            sender.sendMessage(Util.formatString("&cThe player you specified is not online")); return true;
+        }
+
+        String senderName = sender.getName();
+        if (sender instanceof Player)
+            senderName = ((Player) sender).getDisplayName();
+
+        new PotionEffect(PotionEffectType.HEALTH_BOOST, 1, 1).apply(target);
+        Bukkit.getServer().broadcastMessage(Util.formatString("&c" + senderName + "&7 fist bumped &c" + target.getDisplayName()));
+
         return true;
     }
 
