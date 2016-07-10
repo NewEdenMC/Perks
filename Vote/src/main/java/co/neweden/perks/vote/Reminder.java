05f9fc1d197a2b38312b1b1d0ac3c1d3358041f5
aaron <aaron@neweden.co>
2016-07-10 17:42:32 +0100

    Made vote reminder message clickable

--- Vote/src/main/java/co/neweden/perks/vote/Reminder.java
+++ Vote/src/main/java/co/neweden/perks/vote/Reminder.java
@@ -2,7 +2,11 @@ package co.neweden.perks.vote;
 
 import co.neweden.perks.Perks;
 import co.neweden.perks.Util;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -39,13 +43,19 @@ public class Reminder implements Listener {
             ResultSet rs = st.executeQuery();
             if (!rs.next()) return;
             if (rs.getLong("lastVote") == 0 || rs.getLong("lastVote") + reminderPeriod > System.currentTimeMillis() / 1000) return;
-            player.sendMessage(Util.formatStringToBaseComponent(
-                    "&a\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\n" +
-                    " \n" +
-                    "&f&lThe last time you voted was over " + Util.formatTime(reminderPeriod, TimeUnit.SECONDS, false) + " ago, to vote again and earn more " + Perks.getConfigSetting("currency_reference_name", "money") + " for perks, type /vote and vote on the available services.\n" +
-                    " \n" +
-                    "&a\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580"
-            ));
+
+            player.sendMessage(
+                    new ComponentBuilder("\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\n\n")
+                    .color(ChatColor.GREEN)
+                    .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/vote"))
+                    .append("The last time you voted was over " + Util.formatTime(reminderPeriod, TimeUnit.SECONDS, false) + " ago, to vote again and earn more " + Perks.getConfigSetting("currency_reference_name", "money") + " for perks, type /vote or click this message to vote on the available services.\n\n")
+                    .color(ChatColor.WHITE)
+                    .bold(true)
+                    .append("\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580\u2580")
+                    .color(ChatColor.GREEN)
+                    .bold(false)
+                    .create()
+            );
         } catch (SQLException e) {
             Perks.getPlugion().getLogger().log(Level.SEVERE, "An SQL Exception occurred while getting lastVote for player " + player.getUniqueId(), e);
         }
